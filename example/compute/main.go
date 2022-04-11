a9572ee6885ea9db2432867bb4da0faaba5ddd30
Hypo <i@ihypo.net>
2022-04-11 22:50:43 +0800

    update: add example

---
+++ example/compute/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"github.com/hyponet/eventbus/bus"
+)
+
+var waiting = make(chan struct{})
+
+func aAndBComputer(a, b int) {
+	fmt.Printf("%d + %d = %d\n", a, b, a+b)
+	close(waiting)
+}
+
+func main() {
+	_, _ = bus.Register("op.int.and", aAndBComputer)
+	bus.Publish("op.int.and", 1, 2)
+	<-waiting
+}
--- example/wildcard/main.go
+++ example/wildcard/main.go
@@ -1,12 +1,3 @@
-# eventbus
-
-Lightweight eventbus for golang
-
-## Usage
-
-### Static topic
-
-```go
 package main
 
 import (
@@ -14,37 +5,25 @@ import (
 	"github.com/hyponet/eventbus/bus"
 )
 
-func aAndBComputer(a, b int) {
-	fmt.Printf("%d + %d = %d\n", a, b, a+b)
-}
-
-func main() {
-	_, _ = bus.Register("op.int.and", aAndBComputer)
-	bus.Publish("op.int.and", 1, 2)
-}
-```
-
-### Wildcard topic
-
-```go
-package main
-
-import (
-	"fmt"
-	"github.com/hyponet/eventbus/bus"
+var (
+	waitBob   = make(chan struct{})
+	waitAlice = make(chan struct{})
 )
 
 func bobDoSomething() {
 	fmt.Println("Bob do something")
+	close(waitBob)
 }
 
 func aliceDoSomething() {
 	fmt.Println("Alice do something")
+	close(waitAlice)
 }
 
 func main() {
 	_, _ = bus.Register("partner.bob.do", bobDoSomething)
 	_, _ = bus.Register("partner.alice.do", aliceDoSomething)
 	bus.Publish("partner.*.do")
+	<-waitBob
+	<-waitAlice
 }
-```
